4c87cb187cab34f03526ca3f12d0672ec18ec886
serraogt <85106072+serraogt@users.noreply.github.com>
2023-11-08 16:45:34 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,27 +1,30 @@
 from mesa import Model
-from agent import MoneyAgent
-from mesa.time import RandomActivation
 from mesa.space import MultiGrid
+from mesa.time import RandomActivation
+from agent import HappinessAgent
 
-class MoneyModel(Model):
-    """a model with some number of agents""" 
-    def __init__(self, N, width, height ):
+class HappinessModel(Model):
+    """A model with some number of agents."""
+
+    def __init__(self, N, width, height):
         self.num_agents = N
         self.grid = MultiGrid(width, height, True)
         self.schedule = RandomActivation(self)
 
-        """Create agents"""
-        
+        # Create agents
         for i in range(self.num_agents):
-            a = MoneyAgent(i, self)
+            a = HappinessAgent(i, self)
             self.schedule.add(a)
 
-            """add some agents to a random grid cell"""    
+            # Add some agents to a random grid cell
             x = self.random.randrange(self.grid.width)
             y = self.random.randrange(self.grid.height)
-            self.grid.place_agent(a,(x,y))
+            self.grid.place_agent(a, (x, y))
 
     def step(self):
-        """Advance the model by one step"""
         self.schedule.step()
- 
+
+# Example usage:
+model = HappinessModel(100, 10, 10)
+for _ in range(10):
+    model.step()
