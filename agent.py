9e0460de156ea9177ccd752443f253a7f51cb7f4
serraogt <85106072+serraogt@users.noreply.github.com>
2023-11-08 16:45:05 +0300

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,23 +1,34 @@
 from mesa import Agent
 
-class MoneyAgent(Agent):
-    """an agent with fixed initial money of 1 unit"""
+class HappinessAgent(Agent):
+    """an agent with initial happiness"""
 
     def __init__(self,unique_id,model):
         super().__init__(unique_id,model)
-        self.wealth = 1
+        self.age=self.random.randint(18, 80)
+        self.happiness = self.getHappiness()
+
+    def getHappiness(self):
+        baseHappiness=self.random.randint(3,7)/10
+        #start with average happiness
+        happiness= baseHappiness + ((50-self.age)/100)
+        if (happiness>0.8):
+            happiness=0.8
+        elif (happiness<0.2):
+            happiness=0.2    
+        return happiness   
 
     def step(self) -> None: #returns none
         self.move()
        # if self.wealth > 0:
-        self.give_money()
+       # self.give_money()
 
     def give_money(self):
         cellmates = self.model.grid.get_cell_list_contents([self.pos])
         if len(cellmates) > 1:
             other = self.random.choice(cellmates)
             other.wealth += 1
-            self.wealth -= 1
+            self.happiness -= 1
 
     def move(self) -> None:
         possible_steps = self.model.grid.get_neighborhood(self.pos, moore=True, include_center=False)       
