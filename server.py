d3cee32c1249bd0e6e42c803ef76cbc7141a80c3
serraogt <85106072+serraogt@users.noreply.github.com>
2023-11-08 16:44:46 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,29 +1,28 @@
 from mesa import Model
-from agent import MoneyAgent
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
-from model import MoneyModel
+from model import HappinessModel
 
 
 def agent_portrayal(agent):
     portrayal = {"Shape": "circle", "Filled": "true", "r": 0.5}
 
-    if agent.wealth == 0:
+    if agent.getHappiness() == 0.5:
         portrayal["Color"] = "yellow"
         portrayal["Layer"] = 0
         portrayal["r"] = 0.4
-    elif agent.wealth > 0:
+    elif agent.getHappiness() > 0.5:
         portrayal["Color"] = "green"
-        portrayal["Layer"] = 0
-    elif agent.wealth < 0:
+        portrayal["Layer"] = 1
+    elif agent.getHappiness() < 0.5:
         portrayal["Color"] = "red"
-        portrayal["Layer"] = 0
+        portrayal["Layer"] = 2
         portrayal["r"] = 0.2
 
     return portrayal
 
 
 grid = CanvasGrid(agent_portrayal, 10, 10, 500, 500)
-server = ModularServer(MoneyModel, [grid], "Money Model", {"N":100, "width":10, "height":10})
+server = ModularServer(HappinessModel, [grid], "Happiness Model", {"N":100, "width":10, "height":10})
 server.port = 8521  # You can change the port if needed
 server.launch()
